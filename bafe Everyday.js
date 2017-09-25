28baf82892b42a80308ffd16e842d5996e0068a8
Connorc666 <Connorc0627@gmail.com>
2017-09-25 08:30:30 -0500

    Add files via upload

---
+++ bafe Everyday.js
@@ -0,0 +1,6 @@
+javascript:
+window.location.href = 'Order.aspx?DT=2017/10/05';
+var bafe = document.getElementsByTagName("input");
+bafe = '' + bafe[14].onclick;
+bafe = bafe.slice(52,76);
+window.location.href = bafe;
